5a29138385e99bdb35293fefbd6803dda8fc5c0e
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-04-28 13:21:16 -0400

    Reorganize folder structure

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ data_folder=$1
 pairs_list="$(pwd)/PairList.txt"
 pdb_sizes="$(pwd)/PDBSize.txt"
 echo "Running on $1"
-python $(pwd)/MultipleSiteAlignment/Pairs.py $data_folder
-python $(pwd)/MultipleSiteAlignment/PDBSize.py $data_folder
-mpiexec -n 4 python $(pwd)/MultipleSiteAlignment/pocket_matrix_mpi7.py $data_folder $pairs_list $pdb_sizes
+python $(pwd)/site_motif/MultipleSiteAlignment/Pairs.py $data_folder
+python $(pwd)/site_motif/MultipleSiteAlignment/PDBSize.py $data_folder
+mpiexec -n 4 python $(pwd)/site_motif/MultipleSiteAlignment/pocket_matrix_mpi7.py $data_folder $pairs_list $pdb_sizes
