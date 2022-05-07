ad709cd162f7347eaecc4645584ad24857a37c8c
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-05-07 00:47:42 -0400

    Add output directory option to each step of site-motif

--- site_motif/MultipleSiteAlignment/Pairs.py
+++ site_motif/MultipleSiteAlignment/Pairs.py
@@ -1,15 +1,17 @@
 import os
 import sys
 
-if len(sys.argv) == 2:
-    fold = sys.argv[1]
+if len(sys.argv) == 3:
+    site_folder = sys.argv[1]
+    output_folder = sys.argv[2]
 else:
-    print('Pairs.py <Site-Folder>')
+    print('Pairs.py <Site-Folder> <Output-Folder>')
     sys.exit()
 
 dire = os.getcwd()
-out = open('PairList.txt', 'w')
-for i in os.listdir(dire+'/'+fold):
-    for j in os.listdir(dire+'/'+fold):
-        out.write(i+'\t'+j+'\n')
-out.close()
+output_file = os.path.join(output_folder, 'PairList.txt')
+
+with open(output_file, 'w') as out:
+    for i in os.listdir(dire+'/'+ site_folder):
+        for j in os.listdir(dire+'/'+ site_folder):
+            out.write(i + '\t' + j + '\n')
