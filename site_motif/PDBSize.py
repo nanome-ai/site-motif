4cc22900a09c05ac3f7f6917233bbd869a71d651
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-05-30 23:04:15 -0400

    update PDBSize

--- site_motif/PDBSize.py
+++ site_motif/PDBSize.py
@@ -2,33 +2,20 @@ import os
 import sys
 
 if len(sys.argv) == 3:
-	fold = sys.argv[1]
+	site_dir = sys.argv[1]
 	output_dir = sys.argv[2]
 else:
 	print("pdb_res.py <input_folder>")
 	sys.exit()
-
-dire = os.getcwd()
-
-a1 = dire+"/"+fold
-def a():
-	dic = {}
-	out = open(f"{output_dir}/PDBSize.txt",'w')
-	for i in os.listdir(a1):
-		# print(i)
-		aline = open(a1+"/"+i, 'r').readlines()
-		c = 0
-		for line in aline:
-			line = line.strip()
-			if line[:4] == "ATOM":
-				if line[13:15] == "CA":
-					c += 1
-		# print(c)
-		dic[i] = c
-		out.write(i+"\t"+str(c)+"\n")
-	out.close()
-	return dic
-	# out.close()
-dic = a()
-
-
+output_path = os.path.join(output_dir, "PDBSize.txt")
+with open(output_path,'w') as out:
+	for pdb_file in os.listdir(site_dir):
+		pdb_path = os.path.join(site_dir, pdb_file)
+		pdb_lines = open(pdb_path, 'r').readlines()
+		alpha_carbon_count = 0
+		for line in pdb_lines:
+			line_split = line.strip().split()
+			if line_split[0] == "ATOM":
+				if line_split[2] == "CA":
+					alpha_carbon_count += 1
+		out.write(pdb_file + "\t" + str(alpha_carbon_count) + "\n")
