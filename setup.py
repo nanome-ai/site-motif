b525e1209ae615d6e9a26a9ab7e5a4c65be541c7
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-06-20 18:02:32 -0400

    add numpy to requires

--- setup.py
+++ setup.py
@@ -18,4 +18,7 @@ setup(
     url='https://github.com/nanome-ai/site-motif',
     platforms="any",
     keywords=['alignment', 'structural-biology', 'chemistry', 'python'],
+    install_requires=[
+        'numpy',
+    ]
 )
